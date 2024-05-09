c388e56bfa5c6127f9aba28c93c871eed4706d07
feisalmombo <feisalmombo29@gmail.com>
2024-05-09 14:55:29 +0300

    modify jwt middleware

--- app/Http/Middleware/JWTMiddleWare.php
+++ app/Http/Middleware/JWTMiddleWare.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use JWTAuth;
 
 class JWTMiddleWare
 {
@@ -13,8 +14,11 @@ class JWTMiddleWare
      * @param  \Closure  $next
      * @return mixed
      */
-    public function handle($request, Closure $next)
+
+    public function handle(Request $request, Closure $next)
     {
+
+        $user = JWTAuth::parseToken()->authenticate();
         return $next($request);
     }
 }
