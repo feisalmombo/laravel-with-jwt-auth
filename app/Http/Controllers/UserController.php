f86a146a98966df2ca29dfd9826953f7fb967e56
feisalmombo <feisalmombo29@gmail.com>
2024-05-09 14:52:39 +0300

    create jwt middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,4 +30,36 @@ class UserController extends Controller
         ],201);
     }
 
+
+    public function login(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+        try {
+            if (!$token = JWTAuth::attempt($credentials)) {
+                return response()->json([
+                    'error' => 'Invalid Credentials'
+                ], 401);
+            }
+        } catch (JWTException $e) {
+            return response()->json([
+                'error' => 'Could not create token'
+            ], 500);
+        }
+        return response()->json([
+            'token' => $token
+        ], 200);
+    }
+
+
+    public function getUser()
+    {
+        $user = auth('api')->user();
+        return response()->json(['user'=>$user], 201);
+    }
+
 }
