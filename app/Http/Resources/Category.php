2a9836e9b699dc7d1810c77182b0d003ccc6285f
feisalmombo <feisalmombo29@gmail.com>
2024-05-13 12:28:31 +0300

    WIP

--- app/Http/Resources/Category.php
+++ app/Http/Resources/Category.php
@@ -10,10 +10,16 @@ class Category extends JsonResource
      * Transform the resource into an array.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @return array
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+         return [
+            'id' => $this->id,
+            'cat_name' => $this->cat_name,
+            'cat_status' => $this->cat_status,
+            'created_at' => $this->created_at->format('d/m/Y'),
+            'updated_at' => $this->updated_at->format('d/m/Y'),
+        ];
     }
 }
