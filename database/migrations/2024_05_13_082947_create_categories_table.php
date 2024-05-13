9498e4e49ec5f3bde7890cdf1f2a52100ebd6d57
feisalmombo <feisalmombo29@gmail.com>
2024-05-13 11:35:38 +0300

    WWIP

--- database/migrations/2024_05_13_082947_create_categories_table.php
+++ database/migrations/2024_05_13_082947_create_categories_table.php
@@ -15,6 +15,8 @@ class CreateCategoriesTable extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string('cat_name');
+            $table->tinyInteger('cat_status');
             $table->timestamps();
         });
     }
